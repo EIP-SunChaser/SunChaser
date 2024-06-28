291cd539c9d1f8f41911301e99108739f9162838
Polo <jules.trolle@epitech.eu>
2024-06-28 14:54:59 +0200

    [ADD] full jump quand espace est pressed + ajout d'un ui de quête

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -81,7 +81,7 @@ func do_physics_process(delta):
 			velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Handle Speed
