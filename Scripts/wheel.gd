a2b065538bf0ac951eb874a578d9213bb3cb347c
4lrick <alrick.rubio@epitech.eu>
2024-06-28 18:03:21 +0200

    [ADD] Suspension spring

--- Scripts/wheel.gd
+++ Scripts/wheel.gd
@@ -2,13 +2,18 @@ extends RayCast3D
 
 @onready var car: RigidBody3D = get_parent().get_parent()
 @onready var wheel = $Wheel
-
+@onready var springMesh = $Spring
 var previous_spring_length: float = 0.0
-
 @export var is_front_wheel: bool
+
+var initial_spring_scale: Vector3
+var initial_spring_position: Vector3
 
 func _ready():
 	add_exception(car)
+	# Store initial spring mesh scale and position
+	initial_spring_scale = springMesh.scale
+	initial_spring_position = springMesh.transform.origin
 
 func _physics_process(delta):
 	var collision_point = get_collision_point()
@@ -19,10 +24,9 @@ func _physics_process(delta):
 		apply_z_force(collision_point)
 		apply_x_force(delta, collision_point)
 		
-		set_wheel_postion(to_local(get_collision_point()).y + car.wheel_radius)
+		set_wheel_position(to_local(get_collision_point()).y + car.wheel_radius)
 	else:
-		set_wheel_postion(-car.suspension_length)
-
+		set_wheel_position(-car.suspension_length)
 	rotate_wheel(delta)
 		
 func apply_z_force(collision_point):
@@ -38,21 +42,18 @@ func apply_x_force(delta, collision_point):
 	var state := PhysicsServer3D.body_get_direct_state(car.get_rid())
 	var tire_velocity := state.get_velocity_at_local_position(global_position - car.global_position)
 	var lateral_velocity: float = dir.dot(tire_velocity)
-	var grip = car.rear_tire_grip
-	
-	if is_front_wheel:
-		grip = car.front_tire_grip
+	var grip = car.rear_tire_grip if not is_front_wheel else car.front_tire_grip
 	
 	var desired_velocity_change = -lateral_velocity * grip
 	var x_force = desired_velocity_change / delta
 	car.apply_force(dir * x_force, collision_point - car.global_position)
 	
-func set_wheel_postion(new_y_position: float):
+func set_wheel_position(new_y_position: float):
 	wheel.position.y = lerp(wheel.position.y, new_y_position, 0.6)
 	
 func rotate_wheel(delta: float):
 	var dir = car.basis.z
-	var rotation_direction = 1 if car.linear_velocity.dot(dir) > 0 else - 1
+	var rotation_direction = 1 if car.linear_velocity.dot(dir) > 0 else -1
 	
 	wheel.rotate_x(rotation_direction * car.linear_velocity.length() * delta)
 		
@@ -67,14 +68,11 @@ func acceleration(collision_point):
 	car.apply_force(accel_dir * torque, point - car.global_position)
 
 func suspension(delta, collision_point):
-		
 	var suspension_dir = global_basis.y
 	
-	var raycast_origin = global_position	
+	var raycast_origin = global_position    
 	var raycast_dest = collision_point
 	var distance = raycast_dest.distance_to(raycast_origin)
-	
-	var contact = collision_point - car.global_position
 	
 	var spring_length = clamp(distance - car.wheel_radius, 0, car.suspension_length)
 	
@@ -93,3 +91,17 @@ func suspension(delta, collision_point):
 	var point = Vector3(raycast_dest.x, raycast_dest.y + car.wheel_radius, raycast_dest.z)
 	
 	car.apply_force(suspension_dir * suspension_force, point - car.global_position)
+
+	update_spring_mesh(spring_length)
+
+func update_spring_mesh(spring_length: float):
+	var max_compression_ratio: float = 0.85
+	var compression_ratio = spring_length / car.suspension_length
+	compression_ratio = clamp(compression_ratio, max_compression_ratio, 1.0)
+	
+	var new_scale = initial_spring_scale * Vector3(1, compression_ratio, 1)
+	springMesh.scale = new_scale
+	
+	var new_position = initial_spring_position
+	new_position.y = initial_spring_position.y - (car.suspension_length - spring_length) / 2
+	springMesh.transform.origin = new_position
