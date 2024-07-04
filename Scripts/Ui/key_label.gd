86009f937a7256da0aceae44c3cbcec1fa206971
Alrick <alrick.rubio@epitech.eu>
2024-07-04 13:40:07 +0200

    [ADD] detect if the controller is used

---
+++ Scripts/Ui/key_label.gd
@@ -0,0 +1,19 @@
+extends Label
+
+var using_keyboard = true
+
+func _ready():
+	update_text()
+
+func _input(event):
+	if event is InputEventKey:
+		using_keyboard = true
+	elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		using_keyboard = false
+	update_text()
+
+func update_text():
+	if using_keyboard:
+		text = "Press E"
+	else:
+		text = "Press X"
