852a0e942a32853948af4d80611276ffb1492675
Polo <jules.trolle@epitech.eu>
2024-07-04 11:05:57 +0200

    [FIX] bug tree count

--- Scripts/Items/CutTree.gd
+++ Scripts/Items/CutTree.gd
@@ -2,12 +2,15 @@ extends Area3D
 
 func action() -> void:
 	var tree = get_parent()
-	tree.position.y = 17.5
-	tree.rotation_degrees.x = 0
-	GlobalVariables.grow_tree += 1
-	if GlobalVariables.grow_tree >= 6:
-		if GlobalVariables.quest_one == GlobalVariables.check_quest.GROW_TREE_TWO:
-			GlobalVariables.quest_one = GlobalVariables.check_quest.END_ONE
+	if tree.position.y != 17.5:
+		tree.position.y = 17.5
+		tree.rotation_degrees.x = 0
+		GlobalVariables.grow_tree += 1
+		if GlobalVariables.grow_tree >= 6:
+			if GlobalVariables.quest_one == GlobalVariables.check_quest.GROW_TREE_TWO:
+				GlobalVariables.quest_one = GlobalVariables.check_quest.END_ONE
+		else:
+			if GlobalVariables.quest_one == GlobalVariables.check_quest.GROW_TREE_TWO:
+				GlobalVariables.quest_one = GlobalVariables.check_quest.GROW_TREE_ONE
 	else:
-		if GlobalVariables.quest_one == GlobalVariables.check_quest.GROW_TREE_TWO:
-			GlobalVariables.quest_one = GlobalVariables.check_quest.GROW_TREE_ONE
+		pass
