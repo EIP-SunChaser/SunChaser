492ba6c12802dd88e3cd0883795a645b0f943df7
4lrick <alrick.rubio@epitech.eu>
2024-07-17 19:31:27 +0200

    [RM] disable gravity for npc

--- Scripts/Entities/npc.gd
+++ Scripts/Entities/npc.gd
@@ -1,20 +1,6 @@
 extends CharacterBody3D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
-
 var health = 100
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	move_and_slide()
-
 
 func _on_body_part_hit(dam):
 	health -= dam
