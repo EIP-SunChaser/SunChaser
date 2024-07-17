e9e11a7a222ccdd4ada4f768e953494aabf8a24b
4lrick <alrick.rubio@epitech.eu>
2024-07-17 19:31:09 +0200

    [FIX] Better pseudo function

--- Scripts/Entities/Pseudo.gd
+++ Scripts/Entities/Pseudo.gd
@@ -1,23 +1,10 @@
 extends Label3D
 
-@onready var multiplayer_synchronizer = $"../MultiplayerSynchronizer"
-
-func _enter_tree():
-	set_multiplayer_authority(str(name).to_int())
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	if !is_multiplayer_authority(): return
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if multiplayer_synchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		var player_number = 1
-		for i in PlayerManager.Players:
-			if PlayerManager.Players[i].id == multiplayer.get_unique_id():
-				if PlayerManager.Players[i].name != "":
-					text = PlayerManager.Players[i].name
-				else:
-					text = "Player" + str(player_number)
-				break
-			player_number += 1
+	var player_number = 0
+	for player_id in PlayerManager.Players:
+		var player = PlayerManager.Players[player_id]
+		if player.id == multiplayer.get_unique_id():
+			text = player.name if player.name != "" else "Player" + str(player_number + 1)
+			return
+		player_number += 1
